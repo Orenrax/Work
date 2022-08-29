76d7baafb0fc1585b0fca79e8bb1eea0bd1ce93b
Orenman <rx@mail.ru>
2022-08-29 17:33:41 +0500

    VER 1.0 29-08 17:33

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите число: summ - 1, max - 2, ");
+
+        int x = console.nextInt();
+        if(x==1){SUMM(console);}
+        else if(x==2){MAX(console);}
+
+
+
+    }
+
+    static void SUMM(Scanner console) {
+        System.out.println("посчитаем сумму");
+        int sum = 0;
+            while (console.hasNextInt()) {
+                int x = console.nextInt();
+                sum = sum + x;
+            }
+            System.out.println(sum);
+
+        }
+
+    static void MAX(Scanner console) {
+        System.out.println("посчитаем максимум");
+        int max = 0;
+            while (console.hasNextInt()) {
+            int x = console.nextInt();
+            if (x > max)
+                max = x;
+            }
+        System.out.println(max);
+    }
+}
