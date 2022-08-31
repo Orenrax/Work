9c7df50f51ac3e53030fbfc36f696f21a7af0d72
Orenman <rx@mail.ru>
2022-08-31 10:39:57 +0500

    добавил классы Al + BeerSong 31:08:22 в 16:50

---
+++ src/Work/Al.java
@@ -0,0 +1,13 @@
+package Work;
+
+public class Al {
+    static void work(){
+        System.out.println("Пора работать или учиться (((((((((");
+    }
+    static void eat(){
+        System.out.println("Пора принимать пищу");
+    }
+    static void walk(){
+        System.out.println("Пора гулять");
+    }
+}
